fea5e160b3a5a5b3c97721944fd3feb5cc40f077
Chebicheb Kheireddine <76478942+chebichebkheireddine@users.noreply.github.com>
2024-03-13 00:21:15 +0100

    connect data with migration file

---
+++ database/migrations/2024_03_12_174548_create_labs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labs', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("phone");
+            $table->string("photo");
+            $table->string("address");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labs');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LabController;
+use App\Models\Lab;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get("Labs",function(){
+    return view("labs","LabController@index");
 });
